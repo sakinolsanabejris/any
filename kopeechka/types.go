528737afd4a9fa5a318093a9cdfd48fded7db43e
sakinolsanabejris <sakinolsanabejris@gmail.com>
2025-03-05 02:52:31 +0300

    Update types.go

--- kopeechka/types.go
+++ kopeechka/types.go
@@ -10,7 +10,7 @@ type EmailClient struct {
 	OrderId   int
 	Status    string  `json:"status"`
 	EmailId   string  `json:"id"`
-	Email     string  `json:"mail"`
+	Email     string  `json:"email"`
 	Balance   float32 `json:"balance"`
 	Value     string  `json:"value"`
 }
